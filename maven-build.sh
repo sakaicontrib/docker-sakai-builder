0d5ac86f7e476a7ffa54307bb622504dd4beca69
Matthew Jones <jonespm@gmail.com>
2018-12-08 20:57:52 -0500

    Added some shell scripts to simplfy copy/paste

---
+++ maven-build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+BASEDIR=$(dirname "$SCRIPT")
+
+WORK="${BASEDIR}/work"
+# Where tomcat files will be
+TOMCAT="${WORK}/tomcat"
+# Where files will be deployed
+DEPLOY="${TOMCAT}/deploy"
+
+# If docker creates this directory it does it as the wrong user, so create it first
+# These are on the host so they can be re-used between builds
+mkdir -p "$DEPLOY"
+mkdir -p "$WORK/.m2"
+mkdir -p "$WORK/.npm"
+mkdir -p "$WORK/.config"
+mkdir -p "$WORK/.cache"
+
+# Now build the code
+docker run --rm -it --name sakai-build \
+    -e "MAVEN_OPTS=-XX:+TieredCompilation -XX:TieredStopAtLevel=1" \
+    -e "MAVEN_CONFIG=/tmp/.m2" \
+    -v "${DEPLOY}:/usr/src/deploy" \
+    -v "${WORK}/.m2:/tmp/.m2" \
+    -v "${WORK}/.npm:/.npm" \
+    -v "${WORK}/.config:/.config" \
+    -v "${WORK}/.cache:/.cache" \
+    -v "${PWD}:/usr/src/app" \
+    -u `id -u`:`id -g` \
+    -w /usr/src/app sakai:build \
+    /bin/bash -c "mvn -T 1C -B -P mysql install sakai:deploy -Dmaven.test.skip=true -Dmaven.tomcat.home=/usr/src/deploy -Dsakai.cleanup=true -Duser.home=/tmp/" 
