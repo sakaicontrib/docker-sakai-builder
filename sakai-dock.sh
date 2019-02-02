ca0b6b09f7a7d7102e73d368d3107ae4eb07c27f
Matthew Jones <jonespm@gmail.com>
2019-02-02 18:36:49 -0500

    Moved maven build commands into sakai-dock, added property

--- sakai-dock.sh
+++ sakai-dock.sh
@@ -47,12 +47,46 @@ start_mysql() {
 	    -d mysql:5.6 || docker start sakai-mysql
 }
 
+build_sakai() {
+	# If docker creates this directory it does it as the wrong user, so create it first
+	# These are on the host so they can be re-used between builds
+	mkdir -p "$DEPLOY"
+	mkdir -p "$WORK/.m2"
+	mkdir -p "$WORK/.npm"
+	mkdir -p "$WORK/.config"
+	mkdir -p "$WORK/.cache"
+
+	# Now build the code
+	docker run --rm -it --name sakai-build \
+	    -e "MAVEN_OPTS=-XX:+TieredCompilation -XX:TieredStopAtLevel=1" \
+	    -e "MAVEN_CONFIG=/tmp/.m2" \
+	    -v "${DEPLOY}:/usr/src/deploy" \
+	    -v "${WORK}/.m2:/tmp/.m2" \
+	    -v "${WORK}/.npm:/.npm" \
+	    -v "${WORK}/.config:/.config" \
+	    -v "${WORK}/.cache:/.cache" \
+	    -v "${PWD}:/usr/src/app" \
+	    -u `id -u`:`id -g` \
+	    -w /usr/src/app sakai:build \
+	    /bin/bash -c "mvn -T 1C -B -P mysql clean install sakai:deploy -Dmaven.test.skip=true -Dmaven.tomcat.home=/usr/src/deploy -Dsakai.cleanup=true -Duser.home=/tmp/"
+}
+
+clean_deploy() {
+	rm -rf $DEPLOY
+}
+
+set -x
+
 if [ "$1" = "tomcat" ]; then
 	start_tomcat
 elif [ "$1" = "mysql" ]; then
 	start_mysql	
+elif [ "$1" = "build" ]; then
+	build_sakai	
+elif [ "$1" = "clean" ]; then
+	clean_deploy	
 else
-	echo "Must specify mysql or tomcat as arguments"
+	echo "Must specify mysql, tomcat, build or clean as arguments"
 	exit
 fi
 
