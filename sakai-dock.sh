dd0d5b48869f476b59f30b8933f41e40ee471111
Matthew Jones <jonespm@gmail.com>
2023-02-18 15:23:35 -0500

    Add support for https proxy and modi

--- sakai-dock.sh
+++ sakai-dock.sh
@@ -37,16 +37,23 @@ JDK11_OPTS="--add-exports=java.base/jdk.internal.misc=ALL-UNNAMED --add-exports=
 # This may be reconfigured
 JDK11_GC="-Xlog:gc -XX:+UseShenandoahGC -XX:+AlwaysPreTouch"
 
-start_tomcat() {
-    CONTAINER_NAME="sakai-tomcat"
-    CONTAINER_ID=$(docker inspect --format="{{.Id}}" ${CONTAINER_NAME} 2> /dev/null)
+container_check_and_rm() {
+    local CONTAINER_NAME=$1
+    local CONTAINER_ID=$(docker inspect --format="{{.Id}}" ${CONTAINER_NAME} 2> /dev/null)
 
     if [[ "${CONTAINER_ID}" ]]; then
         echo "${CONTAINER_NAME} exists, removing previous instance"
     	docker stop ${CONTAINER_NAME} > /dev/null && docker rm ${CONTAINER_NAME} > /dev/null
     fi
+}
 
-	docker run -d --name=${CONTAINER_NAME} --pull always \
+start_tomcat() {
+    # Startup the https proxy first
+    container_check_and_rm "docker-local-ssl-termination-proxy"
+    docker run -d -e "HOST_IP=127.0.0.1" -e "PORT=8080" -p 443:443 --name="docker-local-ssl-termination-proxy" --rm esplo/docker-local-ssl-termination-proxy
+    
+    container_check_and_rm "sakai-tomcat"
+	docker run -d --name="sakai-tomcat" --pull always \
 	    -p 8080:8080 -p 8089:8089 -p 8000:8000 -p 8025:8025 \
 	    -e "CATALINA_BASE=/usr/src/app/deploy" \
 	    -e "CATALINA_TMPDIR=/tmp" \
@@ -60,26 +67,21 @@ start_tomcat() {
 	    -u `id -u`:`id -g` \
 	    --link sakai-mariadb \
 	    tomcat:9-jdk11-temurin \
-	    /usr/local/tomcat/bin/catalina.sh jpda run || docker start ${CONTAINER_NAME}
+	    /usr/local/tomcat/bin/catalina.sh jpda run || docker start "sakai-tomcat"
 }
 
 start_mariadb() {
 	mkdir -p "${WORK}/mysql/data"
-    CONTAINER_NAME="sakai-mariadb"
-    CONTAINER_ID=$(docker inspect --format="{{.Id}}" ${CONTAINER_NAME} 2> /dev/null)
+    container_check_and_rm "sakai-mariadb"
 
-    if [[ "${CONTAINER_ID}" ]]; then
-        echo "${CONTAINER_NAME} exists, removing previous instance"
-    	docker stop ${CONTAINER_NAME} > /dev/null && docker rm ${CONTAINER_NAME} > /dev/null
-    fi
 	# May want to include an opt for docker rm sakai-mariadb
 	# Start it if we've already created it, unless we want to re-create
-	docker run -p 127.0.0.1:53306:3306 -d --name=${CONTAINER_NAME} --pull always \
+	docker run -p 127.0.0.1:53306:3306 -d --name="sakai-mariadb" --pull always \
 	    -e "MARIADB_ROOT_PASSWORD=sakairoot" \
 	    -v "${WORK}/mysql/scripts:/docker-entrypoint-initdb.d:delegated" \
 	    -v "${WORK}/mysql/data:/var/lib/mysql:delegated" \
 	    -u `id -u`:`id -g` \
-	    -d mariadb:10 --lower-case-table-names=1 || docker start ${CONTAINER_NAME}
+	    -d mariadb:10 --lower-case-table-names=1 || docker start "sakai-mariadb"
 }
 
 # This is mostly for debugging maven/tomcat
