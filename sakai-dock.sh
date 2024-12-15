3f94c59bd5452f921c7a13ca8d09ec7284c44f59
Matthew Jones <jonespm@gmail.com>
2024-12-15 18:12:32 -0500

    Adding support for JDK 17 and Sakai 25

--- sakai-dock.sh
+++ sakai-dock.sh
@@ -24,7 +24,7 @@ DEPLOY="${TOMCAT}/deploy"
 SAKAIHOME="${TOMCAT}/sakaihome"
 
 # Which maven container to use
-MAVEN_IMAGE="markhobson/maven-chrome:jdk-11"
+MAVEN_IMAGE="markhobson/maven-chrome:jdk-17"
 
 # Any extra options to pass to maven
 MVN_EXTRA_OPTS="-Dmaven.plugin.validation=DEFAULT"
@@ -36,11 +36,61 @@ TIMEZONE="America/Detroit"
 
 echo "WORK:$WORK TOMCAT:$TOMCAT DEPLOY:$DEPLOY WICKET_CONFIG:$WICKET_CONFIG"
 
-# Opts from SAK-33595 indicated for JDK11
-JDK11_OPTS="--add-exports=java.base/jdk.internal.misc=ALL-UNNAMED --add-exports=java.base/sun.nio.ch=ALL-UNNAMED --add-exports=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED --add-exports=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED --add-exports=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED --illegal-access=permit"
+CATALINA_OPTS="-server \
+               -Djava.awt.headless=true \
+               -XX:+UseCompressedOops \
+               -XX:+AlwaysPreTouch \
+               -XX:+DisableExplicitGC \
+               -Djava.net.preferIPv4Stack=true"
 
-# This may be reconfigured or removed
-JDK11_GC="-Xlog:gc -XX:+UseShenandoahGC -XX:+AlwaysPreTouch"
+CATALINA_OPTS="$CATALINA_OPTS -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false"
+
+# Jasper config
+CATALINA_OPTS="$CATALINA_OPTS -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false"
+
+# Enable Sakai demo mode
+CATALINA_OPTS="$CATALINA_OPTS -Dsakai.demo=true"
+
+# Sets the http agent
+CATALINA_OPTS="$CATALINA_OPTS -Dhttp.agent=Sakai"
+
+# Timezone some examples for the U.S.:  US/Eastern, US/Central, US/Mountain, US/Pacific, US/Arizona
+CATALINA_OPTS="$CATALINA_OPTS -Duser.timezone=US/Eastern"
+
+# Sets the cookie name
+CATALINA_OPTS="$CATALINA_OPTS -Dsakai.cookieName=SAKAIID"
+
+#Skip scanning serializer.jar
+CATALINA_OPTS="$CATALINA_OPTS -Dtomcat.util.scan.StandardJarScanFilter.jarsToSkip=serializer.jar"
+
+# Opts from SAK-33595 indicated for JDK11
+JAVA_OPTS="--add-opens=java.base/jdk.internal.access=ALL-UNNAMED \
+        --add-opens=java.base/jdk.internal.misc=ALL-UNNAMED \
+        --add-opens=java.base/sun.nio.ch=ALL-UNNAMED \
+        --add-opens=java.base/sun.util.calendar=ALL-UNNAMED \
+        --add-opens=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED \
+        --add-opens=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED \
+        --add-opens=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED \
+        --add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED \
+        --add-opens=java.base/java.io=ALL-UNNAMED \
+        --add-opens=java.base/java.nio=ALL-UNNAMED \
+        --add-opens=java.base/java.net=ALL-UNNAMED \
+        --add-opens=java.base/java.util=ALL-UNNAMED \
+        --add-opens=java.base/java.util.concurrent=ALL-UNNAMED \
+        --add-opens=java.base/java.util.concurrent.locks=ALL-UNNAMED \
+        --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED \
+        --add-opens=java.base/java.lang=ALL-UNNAMED \
+        --add-opens=java.base/java.lang.invoke=ALL-UNNAMED \
+        --add-opens=java.base/java.math=ALL-UNNAMED \
+        --add-opens=java.sql/java.sql=ALL-UNNAMED \
+        --add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
+        --add-opens=java.base/java.time=ALL-UNNAMED \
+        --add-opens=java.base/java.text=ALL-UNNAMED \
+        --add-opens=java.management/sun.management=ALL-UNNAMED \
+        --add-opens=java.desktop/java.awt.font=ALL-UNNAMED \
+        --add-opens=java.desktop/javax.swing.tree=ALL-UNNAMED"
+
+JAVA_DEBUG_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8089 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false" 
 
 container_check_and_rm() {
     local CONTAINER_NAME=$1
@@ -64,7 +114,8 @@ start_tomcat() {
 	    -p 8080:8080 -p 8089:8089 -p 8000:8000 -p 8025:8025 \
 	    -e "CATALINA_BASE=/usr/src/app/deploy" \
 	    -e "CATALINA_TMPDIR=/tmp" \
-	    -e "JAVA_OPTS=-server -Xms2g -Xmx2g -Djava.awt.headless=true -XX:+UseCompressedOops -Dhttp.agent=Sakai -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false” -Dsakai.home=/usr/src/app/deploy/sakai/ -Duser.timezone=${TIMEZONE} -Dsakai.cookieName=SAKAI2SESSIONID -Dsakai.demo=true -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8089 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dwicket.configuration=${WICKET_CONFIG} ${JDK11_OPTS} ${JDK11_GC}" \
+        -e "CATALINA_OPTS=${CATALINA_OPTS}" \
+	    -e "JAVA_OPTS=-Xms2g -Xmx2g -Dsakai.home=/usr/src/app/deploy/sakai/ ${JAVA_DEBUG_OPTS} -Dwicket.configuration=${WICKET_CONFIG} ${JAVA_OPTS}" \
 	    -e "JPDA_ADDRESS=*:8000" \
 	    -v "${DEPLOY}:/usr/src/app/deploy" \
 	    -v "${SAKAIHOME}:/usr/src/app/deploy/sakai" \
@@ -73,7 +124,7 @@ start_tomcat() {
 	    -v "${TOMCAT}/deploy/webapps/ROOT:/usr/src/app/deploy/webapps/ROOT" \
 	    -u `id -u`:`id -g` \
 	    --link sakai-mariadb \
-	    tomcat:9-jdk11-temurin \
+	    tomcat:9-jdk17-temurin \
 	    /usr/local/tomcat/bin/catalina.sh jpda run || docker start "sakai-tomcat"
 }
 
